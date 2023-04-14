e5868cf31599e88b47540744ef6f40804e12d284
joaodejesus <joao.st.jesus@gmail.com>
2023-04-14 15:16:50 -0300

    initial commit

---
+++ hello/src/academy/devdojo/maratonajava/introduction/Hello.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class Hello {
+    //Print hello, world! in console.
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ hello/src/academy/devdojo/maratonajava/introduction/PrimitiveTypes.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introduction;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long e boolean.
+        int age = 27;
+        System.out.println("My age is " +age+ " years old");
+    }
+}
