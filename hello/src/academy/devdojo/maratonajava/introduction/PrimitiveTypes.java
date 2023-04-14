8d629f8f77a7a8d7d10aa867bbdf927db401c63e
joaodejesus <joao.st.jesus@gmail.com>
2023-04-14 17:43:43 -0300

    Adding all the primitive types

--- hello/src/academy/devdojo/maratonajava/introduction/PrimitiveTypes.java
+++ hello/src/academy/devdojo/maratonajava/introduction/PrimitiveTypes.java
@@ -4,6 +4,20 @@ public class PrimitiveTypes {
     public static void main(String[] args) {
         //int, double, float, char, byte, short, long e boolean.
         int age = 27;
+        double salaryDouble = 2000;
+        float salaryFloat = 2500;
+        char character = '\u0394';
+        byte salaryByte = 127;
+        short salaryShort = 10;
+        long salaryLong = 100000;
+        boolean logicTrue = true;
+        boolean logicFalse = false;
+
+        String quote = "Write once, run anywhere!";
+
         System.out.println("My age is " +age+ " years old");
+        System.out.println(logicTrue);
+        System.out.println("Char: " +character);
+        System.out.println(quote);
     }
 }
